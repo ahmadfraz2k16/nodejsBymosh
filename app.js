730363daa2a4dc381c0454e0f50aa946a41bc57d
unknown <ahmadfraz2k16@gmail.com>
2024-07-25 20:40:17 +0500

    nodejs global objects concepts

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-function sayHello(name){
-    console.log("js" + name);
-}
-// sayHello("node");
-// console.log(window);
+//global objects in JS and NodeJS
+console.log();
+setTimeout()
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+var message = '';
+window.message  // we do it in JS, all above functions are prefixed with window. message var is globally available in JS
+//NodeJS 
+//global objects are prefixed with global. e.g. global.console.log()
+global.console.log(message)  //it will show undefined, bcz variables are not global in NodeJS
+// can do in node
+global.setTimeout(); 
+global.clearTimeout();
+global.setInterval();
+global.clearInterval();
