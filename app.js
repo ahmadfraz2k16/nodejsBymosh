74af10828bd95ff2e2468338b33bb495ae038106
unknown <ahmadfraz2k16@gmail.com>
2024-07-25 23:28:25 +0500

    section 2, lecture 14, how to load module

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(module) //you can now access logger here, because it made public in logger.js
+// var logger = require('./logger') //you can now access logger here, because it made public in logger.js
+// console.log(logger)
+// logger.log('logged message from app.js using logger.js')
+
+
+// //its better to use const instead of var, by using const it can be changed accidently
+// var logger = require('./logger') 
+// logger = 1 // assuming you have changed it accidently, you will have error
+// logger.log('logged message from app.js using logger.js')
+
+// //its better to use const instead of var, by using const it can be changed accidently
+// const logger = require('./logger') 
+// logger = 1 // assuming you have changed it accidently, you will have error
+// logger.log('logged message from app.js using logger.js')
+
+//logger is no longer object, because we are now exporting only single function log, in logger.js 'module.exports = log;'
+const logger = require('./logger') 
+//now logger is no longer obj, its a single function from logger.js
+//you can use it like this, logger('message')  // it will call the log function from logger.js
+logger('logged message from app.js using logger.js') //you can assign better name to function e.g. log     matches the actual func in logger.js
--- logger.js
+++ logger.js
@@ -5,6 +5,10 @@ function log(message){
     console.log(message)
 }
 
-//as variables and functions are only scoped to this file (means private members) 
-//you need to make it public to access in other files
-module.exports.log = log; //now this function is listed in export key of module json array, and accessible in other files
+// //as variables and functions are only scoped to this file (means private members) 
+// //you need to make it public to access in other files
+// module.exports.log = log; //now this function is listed in export key of module json array, and accessible in other files
+
+//currently you are exporting a single function as object, object is usefull if we have multiple methods or properties
+//so we can simply it for single function, only exporting single function
+module.exports = log;
