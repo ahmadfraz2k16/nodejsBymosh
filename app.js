9a0d43bbfa5acac5b3d15b8e4e4c9a903bda3752
unknown <ahmadfraz2k16@gmail.com>
2024-07-25 21:12:05 +0500

    section 2, modules, lecture 12

--- app.js
+++ app.js
@@ -1,18 +1,2 @@
-//global objects in JS and NodeJS
-console.log();
-setTimeout()
-clearTimeout();
-
-setInterval();
-clearInterval();
-
-var message = '';
-window.message  // we do it in JS, all above functions are prefixed with window. message var is globally available in JS
-//NodeJS 
-//global objects are prefixed with global. e.g. global.console.log()
-global.console.log(message)  //it will show undefined, bcz variables are not global in NodeJS
-// can do in node
-global.setTimeout(); 
-global.clearTimeout();
-global.setInterval();
-global.clearInterval();
+console.log(module) //module is not global
+//output will be json file with different keys
